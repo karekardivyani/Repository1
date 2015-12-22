79895a61ac6215b5c0a5ef50c1a2cf4c635a4f99
karekardivyani <karekardivyani@gmail.com>
2015-12-22 10:26:25 +0530

    Version2

--- Queue/src/Main.java
+++ Queue/src/Main.java
@@ -2,6 +2,12 @@
 public class Main {
 public static void main(String args[])
 {
-	QueueArrays QArray= new QueueArrays();
+	QueueArrays QArray= new QueueArrays(3);
+	QueueArrayList QArray1=new QueueArrayList();
+	
+	QArray.Enqueue(10);
+	QArray.Enqueue(20);
+	QArray.Dequeue();
+	QArray.display();
 }
 }
--- Queue/src/QueueArrays.java
+++ Queue/src/QueueArrays.java
@@ -1,6 +1,71 @@
-
+import java.util.*;
 public class QueueArrays {
  int queue[];
  int front,rear,size,len;
+ 
+ public QueueArrays(int n)
+ {
+	 size=n;
+	 queue=new int[size];
+	 front=-1;
+	 rear=-1;
+	 len=0;
+	 
+ }
+ public boolean IsEmpty()
+ {
+	 return front == -1;
+ }
+	public boolean IsFull()
+	{
+		return front==0 && rear == size -1;
+	}
 	
+	public void Enqueue(int data)
+	{
+		if (rear == -1) 
+        {
+            front = 0;
+            rear = 0;
+            queue[rear] = data;
+        }
+        else if (rear + 1 >= size)
+            throw new IndexOutOfBoundsException("Overflow Exception");
+        else if ( rear + 1 < size)
+            queue[++rear] = data; 
+	        
+	        len++ ;
+	}
+	
+	public void Dequeue()
+	{
+		if (IsEmpty())
+	           throw new NoSuchElementException("Underflow Exception");
+	        else 
+	        {
+	        	len--;
+	            int data = queue[front];
+	            if ( front == rear) 
+	            {
+	                front = -1;
+	                rear = -1;
+	            }
+	            else
+	                front++;                
+	          
+	}
+	}
+	 public void display()
+	    {
+	        System.out.print("\nQueue = ");
+	        if (len == 0)
+	        {
+	            System.out.print("Empty\n");
+	            return ;
+	        }
+	        for (int i = front; i <= rear; i++)
+	            System.out.print(queue[i]+" ");
+	        System.out.println();        
+	    }
+
 }
